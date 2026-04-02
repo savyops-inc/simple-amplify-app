8c92985c0280e3bce1dfc6279b5617888425cf87
Tokede Akinkunmi <98185157+crownhero@users.noreply.github.com>
2026-04-02 17:46:11 +0100

    Initial commit: Add React frontend and Amplify build spec

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to SavyOps Amplify App</h1>
+        <p>Frontend and Backend integrated.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
